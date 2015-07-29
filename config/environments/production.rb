a4e1d28409c2a1e9cad75df164207f51b743f319
Jones Vitolo <jsvitolo@gmail.com>
2015-07-29 17:38:04 -0300

    Change domain for herokuapp.com production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'jsvitolo-sample-app.heroku.com'
+  host = 'jsvitolo-sample-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :addresses            => 'smtp.sendgrid.net',
